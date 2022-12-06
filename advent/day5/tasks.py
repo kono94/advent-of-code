59f769845cb8af21edd3059c164407db521c7ce5
Jan Loewenstrom <you@example.com>
2022-12-06 23:37:02 +0100

    remove redundant code from day 5

--- advent/day5/tasks.py
+++ advent/day5/tasks.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from collections import deque
 
 @task_output
-def main():
-    splt = Path("advent/day5/input1.txt").read_text().split("\n\n")
+def common(task_specific_function):
+    splt = Path("advent/day5/input.txt").read_text().split("\n\n")
     container_input_lines = splt[0].split("\n 1")[0].split("\n")
     moveset_input_lines = splt[1].split("\n")
     containers = [deque() for i in range(0,9)]
@@ -22,12 +22,8 @@ def main():
         from_stack_idx = int(move_splt[3]) - 1
         to_stack_idx =  int(move_splt[-1]) - 1
         
-        chunk = []
-        for _ in range(0, amount):
-            chunk.append(containers[from_stack_idx].pop())
-        for _ in range(0, amount):
-            containers[to_stack_idx].append(chunk.pop())
-    
+        task_specific_function(containers, amount, from_stack_idx, to_stack_idx)
+       
     top_containers_output = ""
     for c in containers:
         top_containers_output += c.pop()
@@ -35,5 +31,25 @@ def main():
     return top_containers_output
 
 
+
+def task1():
+    def task1_function(containers, amount, from_stack_idx, to_stack_idx):
+        for _ in range(0, amount):
+            containers[to_stack_idx].append(containers[from_stack_idx].pop())
+    
+    return common(task1_function)
+
+
+def task2():
+    def task2_function(containers, amount, from_stack_idx, to_stack_idx):
+        chunk = []
+        for _ in range(0, amount):
+            chunk.append(containers[from_stack_idx].pop())
+        for _ in range(0, amount):
+            containers[to_stack_idx].append(chunk.pop())
+            
+    return common(task2_function)
+
 if __name__ == "__main__":
-    main()
+    task1()
+    task2()
