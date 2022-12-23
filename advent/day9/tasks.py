1e25b7d70313467a066cbb19729c0d42c7b000d3
Jan Löwenstrom <jan.loewenstrom@web.de>
2022-12-23 19:51:49 +0100

    add solution for day9 challenges 1 and 2

--- advent/day9/tasks.py
+++ advent/day9/tasks.py
@@ -8,7 +8,6 @@ class Direction(Enum):
     DOWN = "D"
     LEFT = "L"
 
-
 def move_by_one(position: np.ndarray, direction) -> None:
     if direction == Direction.UP:
         position[1] += 1
@@ -22,32 +21,27 @@ def move_by_one(position: np.ndarray, direction) -> None:
 
 def tail_tracing(H: np.ndarray, T: np.ndarray) -> None:
     difference = H - T
-    print("diff ", difference)
-    directional_distance = np.abs(np.sum(difference))
-    if directional_distance == 2: # trace in one direction
-        T += np.array(difference / 2, dtype= np.int64)
-        print(T)
+    distance = np.sqrt(difference.dot(difference))
+    # distance when one direction => sqrt(2^2 + 0^2) => 2
+    # diagonal, do nothing => sqrt(1^1 + 1^1) => 1
+    # diagonal, trace H => sqrt(-2^1 + 1^2) => sqrt(5) > 2
+    if distance >= 2:
+        # 2 0  -> 1 0
+        # 0 -2 -> 0 -1
+        # 1 2  -> 1 1
+        T += np.clip(difference, -1, 1)
         return
-    
-    diagonally_distance = np.sum(np.abs(difference))
-    if diagonally_distance == 3: # need to trace diagonally
-        # 1 2 -> 1 1
-        # 1 -2 -> 1 -1
-        # -2 -1 -> -1 -1 
-        # -1 2 -> -1 1
-        # Trick: add +1 to position to get 2, 3... -3 -2 etc
-        # dividing with 2 and casting to int, will result
-        # in 1 and 1.5 (-1.5, -1) being casted to 1 1
-        T += np.array((difference + 1) / 2, dtype=np.int64)
-        print(T)
 
 def common(task_specific: callable):
-    input = read_file_line_by_line("advent/day9/input_test.txt")
+    input = read_file_line_by_line("advent/day9/input.txt")
 
     # X, Y positions.
     # starting position is BOTTOM LEFT!
-    H = np.array([0, 0])
-    T = np.array([0, 0])
+    snake = []
+    head = []
+    head.append(np.array([0, 0]))
+    tails = task_specific()
+    snake = head + tails
 
     for line in input:
         parts = line.split(" ")
@@ -57,26 +51,45 @@ def common(task_specific: callable):
         distance = int(parts[1])
 
         for _ in range(0, distance):
-            move_by_one(H, direction)
-            tail_tracing(H, T)
-
+            for i in range(0, len(snake) - 1):
+                if i == 0: # move head based on input
+                    move_by_one(snake[i], direction)
+                tail_tracing(snake[i], snake[i + 1])
+                yield snake[-1]
     return 0
 
 
 @task_output(challenge=1)
 def task1():
+    visited_fields = set()
+
     def task_specific():
-        pass
-    return common(task_specific)
+        tails = []
+        tails.append(np.array([0,0]))
+        return tails
+    
+    for tail_pos in common(task_specific):
+        visited_fields.add(tuple(tail_pos))
+
+    return len(visited_fields)
 
 
 @task_output(challenge=2)
 def task2():
+    visited_fields = set()
+
     def task_specific():
-        pass
-    return common(task_specific)
-   
-   
+        tails = []
+        for _ in range(0, 9):
+            tails.append(np.array([0,0]))
+        return tails
+
+    for tail_pos in common(task_specific):
+            visited_fields.add(tuple(tail_pos))
+
+    return len(visited_fields)
+
+
 if __name__ == "__main__":
     task1()
     task2()
